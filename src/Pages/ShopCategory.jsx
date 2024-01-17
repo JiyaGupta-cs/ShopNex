a8d20efe3c5659313373548d4a1267ab914c95ee
Sweta Singh <120044039+swetasingh8844@users.noreply.github.com>
2024-01-17 23:39:25 +0530

    Update ShopCategory.jsx

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,38 +1,48 @@
-import React, {useContext} from 'react'
-import './CSS/ShopCategory.css'
+import React, { useContext, useState } from 'react';
+import './CSS/ShopCategory.css';
 import { ShopContext } from '../Context/ShopContext';
-import dropdown_icon from '../Components/Assets/dropdown_icon.png'
-import Item from '../Components/Item/Item'
+import dropdown_icon from '../Components/Assets/dropdown_icon.png';
+import Item from '../Components/Item/Item';
 
-const ShopCategory = (props) => {   
-  // in props the category beign passed in navbar like category=men,women,kids will be passed 
-  const {all_product}=useContext(ShopContext);
+const ShopCategory = (props) => {
+  const { all_product } = useContext(ShopContext);
+  const productsPerPage = 12;
+  const [page, setPage] = useState(1);
+
+  const filteredProducts = all_product.filter(item => item.category === props.category);
+  const totalProducts = filteredProducts.length;
+
+  const handleExploreMore = () => {
+    // Increment the page for the next set
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  const startIndex = (page - 1) * productsPerPage;
+  const endIndex = startIndex + productsPerPage;
+
   return (
     <div className='shop-category'>
-      <img className='shopcategory-banner' src={props.banner} alt="" />
-      <div className="shopcategory-indexSort">
+      <img className='shopcategory-banner' src={props.banner} alt='' />
+      <div className='shopcategory-indexSort'>
         <p>
-          <span>Showing 1-12</span> out of 36 products
+          <span>{`Showing ${startIndex + 1}-${Math.min(endIndex, totalProducts)}`}</span> out of {totalProducts} products
         </p>
-        <div className="shopcategory-sort">
-          Sort By <img src={dropdown_icon} alt="" />
+        <div className='shopcategory-sort'>
+          Sort By <img src={dropdown_icon} alt='' />
         </div>
       </div>
-      <div className="shopcategory-products">
-        {all_product.map((item,i) => {
-          if(props.category===item.category){
-            return <Item key={i} id={item.id}  name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-          }
-          else{
-            return null;
-          }
-        })}
+      <div className='shopcategory-products'>
+        {filteredProducts.slice(startIndex, endIndex).map((item, i) => (
+          <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+        ))}
       </div>
-      <div className="shopcategory-loadmore">
-        Explore More
-      </div>
+      {endIndex < totalProducts && (
+        <div className='shopcategory-loadmore' onClick={handleExploreMore}>
+          Explore More
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ShopCategory
+export default ShopCategory;
