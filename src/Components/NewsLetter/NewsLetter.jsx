454750c1e9e3a969e427c4e4614e98bba3ebfb7a
Maruthi <singhkarthik261@gmail.com>
2024-01-18 14:35:17 +0530

    Changes in newsletter section styles

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import './NewsLetter.css'
+import React from "react";
+import "./NewsLetter.css";
 
 const NewsLetter = () => {
   return (
-    <div className='newsletter'>
+    <div className="newsletter">
       <h1>Get Exclusive Offers On Your Email</h1>
       <p>Subscribe to Our NewsLetter and Stay Updated</p>
       <div>
-        <input type="email" placeholder='Your Email Id' />
+        <input type="email" placeholder="Your Email Id" />
         <button>Subscribe</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewsLetter
+export default NewsLetter;
