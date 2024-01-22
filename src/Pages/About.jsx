05e3c3202b2eb37a13277f8bfb9d6881f69d2b96
prasukjain07 <prasukjain0703@gmail.com>
2024-01-22 23:51:25 +0530

    UI improved

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,6 +3,7 @@ import './CSS/About.css'
 
 const About = () => {
   return (
+    <>
     <div className="container-my">
       <h1 id = "aboutheading">About Us</h1>
       <div className="content">
@@ -11,6 +12,35 @@ const About = () => {
       </p>
     </div>
     </div>
+    <div className='container-2'>
+      <div class="accordion" id="accordionExample">
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Affordable Online Shopping at Your Fingertips
+          </button>
+        </h2>
+        <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+          There are many benefits of shopping online. You can take your time and look at different options to find exactly what you want. It's easy to compare prices online and find exactly what you are looking for. And now with Meesho, you can shop for anything you want at the lowest prices in the market. Even if you want to shop for cool gifts for your friends and family, there are many options that you can find on the Internet.
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          A Huge Collection of Clothes Across All Categories
+          </button>
+        </h2>
+        <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            We have a very wide range of clothes in all categories. For men, women and kids.  In our latest collections, you will find all the popular items at an affordable price, so you can be confident you're getting the best deal. 
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    </>
   );
 };
 
