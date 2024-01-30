0e04518033899472b1a7c9cfa2d092bdf6055111
mhdi-742 <mhgit7427778@gmail.com>
2024-01-30 06:16:17 +0530

    some minnor changes

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -8,7 +8,7 @@ const Item = (props) => {
     return (
         <div className='item'>
             <Link to={`/product/${props.id}`}> <img onClick={window.scrollTo(0,0)} src={props.image} alt="" /></Link>
-            <p className={`pit_${theme}`}>{props.name}</p><p>{props.id}</p>
+            <p className={`pit_${theme}`}>{props.name}</p><p className={`pit_${theme}`}>{props.id}</p>
             <div className="item-prices">
                 <div className={`item-price-new_${theme}`}>${props.new_price}</div>
                 <div className={`item-price-old_${theme}`}>${props.old_price}</div>
