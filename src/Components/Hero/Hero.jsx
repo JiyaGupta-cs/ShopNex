277950061e06a55f1e71834c19cfe41b9d03af05
jiya10208 <jiya102082004@gmail.com>
2024-01-31 12:16:30 +0530

    adding more responsiveness.
    The homepage can be scrolled in both x and y axis  or  we can say leftward or  downward, which does not given very good user experience.
    
    In order to improve the user experience i have basically used flex wrap, rem, em like properties in order to make it a little better.

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,34 +1,35 @@
-import React, { useContext } from 'react'
-import './Hero.css'
-import hand_icon from '../Assets/hand_icon.png'
-import arrow_icon from '../Assets/arrow.png'
-import hero_image from '../Assets/hero_image.png'
-import { ShopContext } from '../../Context/ShopContext'
+import React, { useContext } from "react";
+import "./Hero.css";
+import hand_icon from "../Assets/hand_icon.png";
+import arrow_icon from "../Assets/arrow.png";
+import hero_image from "../Assets/hero_image.png";
+import { ShopContext } from "../../Context/ShopContext";
 
 const Hero = () => {
-    const {theme}=useContext(ShopContext)
-    return (
-        <div className={'hero_'+theme}>
-            <div className="hero-left">
-                <h2  className={'h2h_'+theme}>NEW ARRIVALS ONLY</h2>
-                <div>
-                    <div className="hero-hand-icon">
-                        <p className={'ph_'+theme}>New</p>
-                        <img src={hand_icon} alt="" />
-                    </div>
-                    <p className={'ph_'+theme}>Collections</p>
-                    <p className={'ph_'+theme}>For Everyone</p>
-                </div>
-                <div className="hero-latest-btn">
-                    <div className={'div_'+theme}>Latest Collection</div>
-                    <img src={arrow_icon} alt="" />
-                </div>
-            </div>
-            <div className="hero-right">
-                <img src={hero_image} alt="" />
-            </div>
+  const { theme } = useContext(ShopContext);
+  return (
+    <div className={"hero_" + theme}>
+      <div className="hero-left">
+        <h2 className={"h2h_" + theme}>NEW ARRIVALS ONLY</h2>
+        <div>
+          <div className="hero-hand-icon">
+            <p className={"ph_" + theme}>New</p>
+            {/* 👋 */}
+            <img src={hand_icon} alt="" />
+          </div>
+          <p className={"ph_" + theme}>Collections</p>
+          <p className={"ph_" + theme}>For Everyone</p>
         </div>
-    )
-}
+        <div className="hero-latest-btn">
+          <div className={"div_" + theme}>Latest Collection</div>
+          <img src={arrow_icon} alt="" />
+        </div>
+      </div>
+      <div className="hero-right">
+        <img src={hero_image} alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,22 +1,31 @@
-import React, { useContext } from 'react'
-import './Popular.css'
-import data_product from '../Assets/data'
-import Item from '../Item/Item'
-import { ShopContext } from '../../Context/ShopContext'
+import React, { useContext } from "react";
+import "./Popular.css";
+import data_product from "../Assets/data";
+import Item from "../Item/Item";
+import { ShopContext } from "../../Context/ShopContext";
 
 const Popular = () => {
-    const{theme}=useContext(ShopContext);
-    return (
-        <div className='popular'>
-            <h1 className={`h1p_${theme}`}>POPULAR IN WOMEN</h1>
-            <hr className={`hrp_${theme}`} />
-            <div className="popular-item">
-                {data_product.map((item,i)=>{
-                    return<Item key={i}  id={item.id}   name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-                })}
-            </div>
-        </div>
-    )
-}
+  const { theme } = useContext(ShopContext);
+  return (
+    <div className="popular">
+      <h1 className={`h1p_${theme}`}>POPULAR IN WOMEN</h1>
+      <hr className={`hrp_${theme}`} />
+      <div className="popular-item">
+        {data_product.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Popular
+export default Popular;
