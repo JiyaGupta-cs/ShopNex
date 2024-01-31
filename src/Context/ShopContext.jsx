d29e234bb556fb7f136e28f53c93a8e7f3e319f3
jiya10208 <jiya102082004@gmail.com>
2024-01-31 07:10:48 +0530

    usereducer
    using user reducer to interlink all the state and creating a more efficient code.
    
    This way it makes it more readable and all the state doing similar task are stayed at on place.

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,29 +1,77 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useReducer, useState } from "react";
 import all_product from "../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);
 
+const initialState = {
+  cartItems: [],
+  error: "",
+  total: 0,
+  theme: "dark",
+  length: 0,
+};
+function reducer(state, action) {
+  const newTotal = state.cartItems.reduce(
+    (acc, product) => acc + product.new_price,
+    0
+  );
+  switch (action.type) {
+    case "cart/added":
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload],
+        total: newTotal,
+        length: state.cartItems.length,
+      };
+    case "cart/remove":
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (product) => product.id !== action.payload
+        ),
+
+        total: newTotal,
+        length: state.cartItems.length,
+      };
+
+    case "cart/theme":
+      return {
+        ...state,
+        theme: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
+
 const ShopContextProvider = (props) => {
-  const [cartItems, setcartItems] = useState([]);
-  const [theme,setTheme]=useState("dark");
+  const [{ cartItems, total, theme, length }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
   const addToCart = (itemId, size, quantity) => {
     const cartProduct = all_product.find((product) => product.id === itemId);
     cartProduct.size = size;
     cartProduct.Quantity = quantity;
     console.log(cartProduct);
-    setcartItems([...cartItems, cartProduct]);
+    dispatch({ type: "cart/added", payload: cartProduct });
   };
   const removeFromCart = (itemId) => {
-    setcartItems(cartItems.filter((product) => product.id !== itemId));
+    dispatch({ type: "cart/remove", payload: itemId });
   };
 
   const getTotalCartAmount = () => {
-    const total = 0;
-    return cartItems.map((product) => total + product.new_price)[0] || 0;
+    return total;
   };
 
   const getTotalCartItems = () => {
-    return cartItems.length;
+    return length;
+  };
+  const setTheme = (theme) => {
+    dispatch({ type: "cart/theme", payload: theme });
+    return length;
   };
 
   const contextValue = {
