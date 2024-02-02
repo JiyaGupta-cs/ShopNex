9366c29a143a4245e566a3b781db36559fa4d950
Prithwi Hegde <115262737+Prithwi32@users.noreply.github.com>
2024-02-02 17:47:58 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Hero.css'
 import hand_icon from '../Assets/hand_icon.png'
 import arrow_icon from '../Assets/arrow.png'
 import hero_image from '../Assets/hero_image.png'
+import { ShopContext } from '../../Context/ShopContext'
 
 const Hero = () => {
+    const {theme}=useContext(ShopContext)
     return (
-        <div className='hero'>
+        <div className='hero' {'hero_'+theme} >
             <div className="hero-left common-container">
-                <h2>NEW ARRIVALS ONLY</h2>
+                <h2 className={'h2h_'+theme}>NEW ARRIVALS ONLY</h2>
                 <div>
                     <div className="hero-hand-icon">
-                        <p>New</p>
+                        <p className={'ph_'+theme}>New</p>
                         <img src={hand_icon} alt="" />
                     </div>
-                    <p>Collections</p>
-                    <p>For Everyone</p>
+                    <p className={'ph_'+theme}>Collections</p>
+                    <p className={'ph_'+theme}>For Everyone</p>
                 </div>
                 <div className="hero-latest-btn">
-                    <div>Latest Collection</div>
+                    <div className={'div_'+theme}>Latest Collection</div>
                     <img src={arrow_icon} alt="" />
                 </div>
             </div>
