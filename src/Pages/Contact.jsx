f9080ea34529cb351d8a0d991f4b36a4de400975
Prathamesh Wadgaonkar <wadgaonkarprathamesh@gmail.com>
2024-02-20 20:20:18 +0530

    changed

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -28,8 +28,9 @@ const Contact = () => {
             <form id="contact-form">
               <div className="form-group">
                 <div className="field">
+                <i className="fas fa-user"></i>
                   <input type="text" name="name" placeholder="Name" required />
-                  <i className="fas fa-user"></i>
+                  
                 </div>
                 <div className="field">
                   <input type="text" name="email" placeholder="Email" required />
