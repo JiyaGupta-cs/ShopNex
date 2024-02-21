b01b9f335f4a0bf0c96f1572d6be0964be396366
ramith-kulal <ramithnr1234@gmail.com>
2024-02-21 22:31:05 +0530

    Add empty cart message and style it

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import "./CartItems.css";
 import { ShopContext } from "../../Context/ShopContext";
 import remove_icon from "../Assets/cart_cross_icon.png";
+import placeholder_image from "../Assets/empty-cart.png";
 
 const CartItems = () => {
   const { cartItems, removeFromCart, getTotalCartAmount, theme } =
@@ -11,6 +12,9 @@ const CartItems = () => {
   const calculateTotalPrice = (item) => {
     return item.new_price * item.quantity;
   };
+
+  // Check if cart is empty
+  const isEmptyCart = cartItems.length === 0;
 
   return (
     <div className="cartitems">
@@ -23,17 +27,42 @@ const CartItems = () => {
         <p>Total</p>
         <p>Remove</p>
       </div>
+      {isEmptyCart && (
+        <div className="empty-cart">
+          <img
+            src={placeholder_image}
+            alt="Empty Cart"
+            style={{ width: "150px", marginLeft: "490px" }}
+          />
+          <div style={{ textAlign: "center" }}>
+            <p style={{ fontSize: "24px", color: "#333" }}>
+              Hey, it feels so light!
+            </p>
+            <p style={{ fontSize: "18px", color: "#999" }}>
+              There is nothing in your bag. Let's add some items.
+            </p>
+          </div>
+        </div>
+      )}
       <hr />
       {cartItems.map((item) => {
         return (
           <div key={item.id}>
-            <div className={`cartitems-format cartitems-format-main ci_${theme}`}>
-              <img src={item.image} alt="" className="carticon-product-icon" />
+            <div
+              className={`cartitems-format cartitems-format-main ci_${theme}`}
+            >
+              <img
+                src={item.image}
+                alt=""
+                className="carticon-product-icon"
+                style={{ width: "50px", margin: "auto" }}
+              />
               <p>{item.name}</p>
               <p className="text-Align">${item.new_price}</p>
               <p className="text-Align">{item.size}</p>
               <p className="cartitems-quantity">{item.quantity}</p>
-              <p className="text-Align">${calculateTotalPrice(item)}</p> {/* Display total price for each item */}
+              <p className="text-Align">${calculateTotalPrice(item)}</p>{" "}
+              {/* Display total price for each item */}
               <img
                 className="cartitems-remove-icon"
                 src={remove_icon}
@@ -69,7 +98,7 @@ const CartItems = () => {
           <button>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cartitems-promocode">
-          <p>If you have promocode, Enter it here</p>
+          <p>If you have a promo code, enter it here</p>
           <div className="cartitems-promobox">
             <input type="text" placeholder="Promo Code" />
             <button>Submit</button>
